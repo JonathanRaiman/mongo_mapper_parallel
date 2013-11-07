a24c44e4ae0ceedda40256c3ff672ce67eb0ce9e
Jonathan Raiman <jonathanraiman@gmail.com>
2013-11-07 11:47:03 -0500

    made bug fixes, improvements to documentation, and more

--- lib/mongo_mapper_parallel.rb
+++ lib/mongo_mapper_parallel.rb
@@ -1,4 +1,3 @@
-# @title Mongo Mapper Parallel
 # @author Jonathan Raiman
 require 'parallel'            # for parallel processing
 require 'colorize'            # for colored output
@@ -22,9 +21,10 @@ class MongoMapperParallel
 
 		# A chunk that will be parallelized
 		# 
-		# @param :key [String] the lower bound of the range of resources to retrieve
-		# @param :future_key [String] the upper bound for the range of resources to retrieve
-		# @param :compiler [MongoMapperParallel] the Parallel execution object that holds the keys, javascript, and arguments.
+		# @param [Hash] opts the options to create the chunk.
+		# @option opts [String] :key the lower bound of the range of resources to retrieve
+		# @option opts [String] :future_key the upper bound for the range of resources to retrieve
+		# @option opts [MongoMapperParallel] :compiler the Parallel execution object that holds the keys, javascript, and arguments.
 		#
 		def initialize(opts={})
 			@key        = opts[:key]
@@ -66,10 +66,10 @@ class MongoMapperParallel
 
 	# Obtains the splitVectors keys to find chunks to parallelize via the MongoDB `splitVector` command.
 	#
-	# @return list [Array<MongoMapperParallel::Key>] the list of the keys that will be used for parallel operation
+	# @return [Array<MongoMapperParallel::Key>] the list of the keys that will be used for parallel operation
 	#
 	def get_split_keys
-		@split_keys, splits = [], @command_class.database.command({splitVector: "#{@command_class.database.name}.#{@command_class.collection.name}", keyPattern: {@split.to_sym => 1}, maxChunkSizeBytes: 32*1024*1024 })["splitKeys"]
+		@split_keys, splits = [], @command_class.database.command({splitVector: "#{@command_class.database.name}.#{@command_class.collection.name}", keyPattern: {@split.to_sym => 1}, maxChunkSizeBytes: @splitSize })["splitKeys"]
 		splits.each_with_index do |split_key,k|
 			@split_keys << MongoMapperParallel::Key.new(:compiler => self, :key => split_key[@split.to_s], :future_key => (splits[k+1] ? splits[k+1][@split.to_s] : nil))
 		end
@@ -77,17 +77,20 @@ class MongoMapperParallel
 
 	# Instantiates the parallel operation object with the right class, javascript function, and field
 	#
-	# @param :class [Class] the Mongo collection's Ruby Class to execute operations on.
-	# @param :javascript [String] the Javascript function in String format
-	# @param :args [Array, Hash] the arguments to pass to the Javascript function
-	# @param :split [String, Symbol] the field to split the computation on -- typically an indexed unique property of the resources in the collection.
-	# @return list [Array<MongoMapperParallel::Key>] the list of the keys that will be used for parallel operation.
+	# @param opts [Hash] the options to initialize the parallel script.
+	# @option opts [Class] :class the Mongo collection's Ruby Class to execute operations on.
+	# @option opts [String] :javascript the Javascript function in String format
+	# @option opts [Array, Hash] :args the arguments to pass to the Javascript function
+	# @option opts [String, Symbol] :split the field to split the computation on -- typically an indexed unique property of the resources in the collection.
+	# @option opts [Fixnum] :maxChunkSizeBytes the size of the chunks to parallelize. Defaults to `32*1024*1024 = 33554432`.
+	# @return [MongoMapperParallel]
 	#
 	def initialize(opts={})
 		@command_class = opts[:class]
 		@javascript    = opts[:javascript]
 		@args          = opts[:args]
 		@split         = opts[:split] # name, title, etc...
+		@splitSize     = opts[:maxChunkSizeBytes] || 32*1024*1024
 		get_split_keys()
 		self
 	end
@@ -101,6 +104,20 @@ class MongoMapperParallel
 			JRProgressBar.show(k,total)
 		end
 		puts "Success".green
+	end
+
+	# In case of stalled progress you can skip ahead by a percentage and mark the keys as `completed`.
+	#
+	# @param percentage [Float] how far along you want to advance, a value between 0.0 and 1.0
+	# @return [MongoMapperParallel]
+	def advance percentage
+		if percentage.class != Float
+			raise TypeError.new "Can only advance by a Float value."
+		elsif percentage > 1.0 or percentage < 0.0
+			raise RangeError.new "Can only advance by a Float between 0.0 and 1.0."
+		end
+		@split_keys[0..(@split_keys.length*percentage).to_i].each {|i| i.completed = true}
+		self
 	end
 
 end
